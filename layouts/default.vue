a840539b7e9a32892f51c20f14fe7a665914b8c6
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-06-24 20:06:13 +0200

    Import the custom CSS reset

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,5 @@
 <style lang="scss">
 @import '~normalize-scss/sass/normalize/_import-now.scss';
 @import '~@avalanche/generic-box-sizing-reset';
-@import '~@avalanche/setting-colors';
-@import '../assets/scss/settings/generic';
+@import '../assets/scss/generic/reset';
 </style>
