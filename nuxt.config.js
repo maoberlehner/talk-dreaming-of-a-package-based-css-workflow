d0e2e526db5a0a0e1ce2469294240640a064d8c9
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-06-26 18:45:58 +0200

    Finetune nuxt settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,20 +5,16 @@ module.exports = {
    * Headers of the page.
    */
   head: {
-    title: `starter`,
+    title: `To Do App`,
     meta: [
       { charset: `utf-8` },
       { name: `viewport`, content: `width=device-width, initial-scale=1` },
-      { hid: `description`, name: `description`, content: `` },
-    ],
-    link: [
-      { rel: `icon`, type: `image/x-icon`, href: `/favicon.ico` },
     ],
   },
   /**
    * Customize the progress-bar color.
    */
-  loading: { color: `#3B8070` },
+  loading: { color: `#00acc1` },
   /**
    * Build configuration.
    */
