aa94a4eebdf36f85c2f6db9332b5d7895a00a8fb
Markus Oberlehner <markus.oberlehner@karriere.at>
2017-06-26 11:38:56 +0200

    Update the about page and refactor To Do / homepage

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>About</h1>
+  <div class="o-vertical-spacing o-vertical-spacing--l">
+    <wcf-headline :level="1">About</wcf-headline>
+    <p>Lorem Ipsum dolor sit amet.</p>
+  </div>
 </template>
+
+<script>
+import WcfHeadline from '../../components/Headline.vue';
+
+export default {
+  components: {
+    WcfHeadline,
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="c-rewarder o-vertical-spacing o-vertical-spacing--l">
-    <wcf-headline :level="1" class="c-headline--underlined">To Do</wcf-headline>
-    <div class="c-rewarder__content">
-      <div class="o-grid">
-        <div class="o-grid__item o-vertical-spacing o-vertical-spacing--l u-width-12/12 u-width-6/12@m">
-          <wcf-headline :level="2">incomplete Tasks</wcf-headline>
-          <wcf-task-list :tasks="incompleteTasks"></wcf-task-list>
-          <wcf-task-form></wcf-task-form>
-        </div>
-        <div class="o-grid__item o-vertical-spacing o-vertical-spacing--l u-width-12/12 u-width-6/12@m">
-          <wcf-headline :level="2">completed Tasks</wcf-headline>
-          <wcf-task-list :tasks="completedTasks"></wcf-task-list>
-        </div>
+  <div class="o-vertical-spacing o-vertical-spacing--l">
+    <wcf-headline :level="1">To Do</wcf-headline>
+    <div class="o-grid">
+      <div class="o-grid__item o-vertical-spacing o-vertical-spacing--l u-width-12/12 u-width-6/12@m">
+        <wcf-headline :level="2">incomplete Tasks</wcf-headline>
+        <wcf-task-list :tasks="incompleteTasks"></wcf-task-list>
+        <wcf-task-form></wcf-task-form>
+      </div>
+      <div class="o-grid__item o-vertical-spacing o-vertical-spacing--l u-width-12/12 u-width-6/12@m">
+        <wcf-headline :level="2">completed Tasks</wcf-headline>
+        <wcf-task-list :tasks="completedTasks"></wcf-task-list>
       </div>
     </div>
   </div>
@@ -40,8 +38,8 @@ export default {
   prefetch(store) {
     return fetchTasks(store);
   },
-  beforeMount() {
-    return fetchTasks(this.$store);
+  asyncData() {
+    // return fetchTasks(this.$store);
   },
 };
 </script>
